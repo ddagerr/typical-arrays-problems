95ca512f5ff887e65fd89e93ecf5adc279eff121
ddagerr <gershanok32@gmail.com>
2021-03-05 22:33:41 +0300

    feat: create functions min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array!=undefined&&array.length!=0){
+  let minValue=array[0]
+   for (let i=1;i<array.length;i++){
+      if (array[i]<minValue) minValue=array[i]
+   }
+   return minValue;
 }
+else return 0
+}
+
+
 
 exports.max = function max (array) {
-  return 0;
+  if(array!=undefined&&array.length!=0){
+  let maxValue=array[0]
+   for (let i=1;i<array.length;i++){
+      if (array[i]>maxValue) maxValue=array[i]
+   }
+   return maxValue;
+}
+else return 0
 }
 
+
+
 exports.avg = function avg (array) {
-  return 0;
+    if(array!=undefined&&array.length!=0){
+    let sum=0
+     for (let i=0;i<array.length;i++){
+        sum+=array[i]
+     }
+     return sum/array.length;
+  }
+  else return 0
 }
+  
+ 
